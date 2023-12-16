f35f6b99a46255b7313ee08cdeb6bf88958028f1
Crystalj0nes <146082225+Crystalj0nes@users.noreply.github.com>
2023-12-16 19:33:45 +0800

    Activity

---
+++ Activity/Activity.php
@@ -0,0 +1,40 @@
+<?php
+class Student {
+    private $studentID;
+    private $firstName;
+    private $lastName;
+    private $dateOfBirth;
+    private $email;
+    private $phone;
+
+    // getters and setters for each attribute
+}
+
+class Course {
+    private $courseID;
+    private $courseName;
+    private $credits;
+
+    // getters and setters for each attribute
+}
+
+class Instructor {
+    private $instructorID;
+    private $firstName;
+    private $lastName;
+    private $email;
+    private $phone;
+
+    // getters and setters for each attribute
+}
+
+class Enrollment {
+    private $enrollmentID;
+    private $studentID;
+    private $courseID;
+    private $enrollmentDate;
+    private $grade;
+
+    // getters and setters for each attribute
+}
+?>  
